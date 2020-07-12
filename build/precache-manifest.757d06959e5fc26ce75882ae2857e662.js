227bae0afde026cbb4678f10b18d25ed0c708456
bard.bringeland@gmail.com <bard.bringeland@gmail.com>
2020-07-12 23:08:47 +0200

    Fix

---
+++ build/precache-manifest.757d06959e5fc26ce75882ae2857e662.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94b47ad7b026aeac0cfe804e1f17bbb4",
+    "url": "/routing/index.html"
+  },
+  {
+    "revision": "887168910407d4cdc25e",
+    "url": "/routing/static/css/main.f4a976e0.chunk.css"
+  },
+  {
+    "revision": "b396952576d660347fa3",
+    "url": "/routing/static/js/2.4e0a76ff.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/routing/static/js/2.4e0a76ff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "887168910407d4cdc25e",
+    "url": "/routing/static/js/main.9ea35dd4.chunk.js"
+  },
+  {
+    "revision": "7e09f3dd0f160c3b84a6",
+    "url": "/routing/static/js/runtime-main.26fec33f.js"
+  }
+]);
